3867217a4a3ed4fd7f5bf4d00b49b32614d98a02
Stefano Casasola <ste.casasola@gmail.com>
2021-06-21 11:54:16 +0200

    Completes the lecture

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ import { Col, Container, Row } from 'react-bootstrap'
 
 class App extends Component {
   state = {
-    cart: [],
+    cart: [], // initially cart will always be an empty array
   }
 
   addToCart = (book) => {
     let newCart = [...this.state.cart]
     newCart.push(book)
+    this.setState({
+      cart: newCart,
+    })
+  }
+
+  removeFromCart = (index) => {
+    let newCart = [...this.state.cart.slice(0, index), ...this.state.cart.slice(index + 1)]
     this.setState({
       cart: newCart,
     })
@@ -39,7 +46,13 @@ class App extends Component {
             <BookStore addToCart={this.addToCart} />
           </Route> */}
           <Route path="/" exact render={(routerProps) => <BookStore {...routerProps} addToCart={this.addToCart} />} />
-          <Route path="/cart" exact component={Cart} />
+          <Route
+            path="/cart"
+            exact
+            render={(routerProps) => (
+              <Cart {...routerProps} cart={this.state.cart} removeFromCart={this.removeFromCart} />
+            )}
+          />
         </Container>
       </Router>
     )
